4aa1f2dccf72cf8c2dbd3561f7a91d6ae8f935dd
MAdrid <454434635@qq.com>
2023-10-19 16:49:24 +0800

    update

--- src/main/scala/Write-Back-Stage/ROB.scala
+++ src/main/scala/Write-Back-Stage/ROB.scala
@@ -62,88 +62,177 @@ class ROB_IO(n: Int) extends Bundle{
 
 class ROB(n: Int) extends Module{
     val io = IO(new ROB_IO(n))
+    val neach = n / 4
     import ROB_Pack._
-    val rob = RegInit(VecInit(Seq.fill(n)(0.U.asTypeOf(new rob_t))))
-
-    val head = RegInit(0.U(log2Ceil(n).W))
-    val tail = RegInit(0.U(log2Ceil(n).W))
+    val rob = RegInit(VecInit(Seq.fill(4)(VecInit(Seq.fill(neach)(0.U.asTypeOf(new rob_t))))))
+    val head = RegInit(VecInit(Seq.fill(4)(0.U(log2Ceil(neach).W))))
+    val tail = RegInit(0.U(log2Ceil(neach).W))
     val elem_num = RegInit(0.U((log2Ceil(n)+1).W))
+    val head_sel = RegInit(0.U(2.W))
 
     val empty = elem_num === 0.U
     val insert_num = PopCount(io.inst_valid_rn)
     val full = elem_num + insert_num > n.asUInt
 
-    when(~full){
+    // rn stage
+
+    when(!full){
         for(i <- 0 until 4){
             when(io.inst_valid_rn(i)){
-                rob(tail+i.U).rd := io.rd_rn(i)
-                rob(tail+i.U).rd_valid := io.rd_valid_rn(i)
-                rob(tail+i.U).prd := io.prd_rn(i)
-                rob(tail+i.U).pprd := io.pprd_rn(i)
-                rob(tail+i.U).pc := io.pc_rn(i)
-                rob(tail+i.U).is_store := io.is_store_rn(i)
-                rob(tail+i.U).pred_update_en := io.pred_update_en_rn(i)
-                rob(tail+i.U).rf_wdata := 0.U
-                rob(tail+i.U).real_jump := false.B
-                rob(tail+i.U).predict_fail := false.B
-                rob(tail+i.U).branch_target := 0.U
-                rob(tail+i.U).complete := false.B
+                rob(i)(tail).rd := io.rd_rn(i)
+                rob(i)(tail).rd_valid := io.rd_valid_rn(i)
+                rob(i)(tail).prd := io.prd_rn(i)
+                rob(i)(tail).pprd := io.pprd_rn(i)
+                rob(i)(tail).pc := io.pc_rn(i)
+                rob(i)(tail).is_store := io.is_store_rn(i)
+                rob(i)(tail).pred_update_en := io.pred_update_en_rn(i)
+                rob(i)(tail).rf_wdata := 0.U
+                rob(i)(tail).real_jump := false.B
+                rob(i)(tail).predict_fail := false.B
+                rob(i)(tail).branch_target := 0.U
+                rob(i)(tail).complete := false.B
             }
         }
     }
 
-    tail := Mux(io.predict_fail_cmt, head + PopCount(io.cmt_en), Mux(!full && !io.stall, tail + insert_num, tail))
-    elem_num := Mux(io.predict_fail_cmt, 0.U, Mux(!full && !io.stall, elem_num + insert_num - PopCount(io.cmt_en), elem_num - PopCount(io.cmt_en)))
     for(i <- 0 until 4){
-        io.rob_index_rn(i) := tail + i.U
+        io.rob_index_rn(i) := tail ## i.U(2.W)
     }
-    
+
     // wb stage
     for(i <- 0 until 4){
         when(io.inst_valid_wb(i)){
-            rob(io.rob_index_wb(i)).complete := true.B
-            rob(io.rob_index_wb(i)).predict_fail := io.predict_fail_wb(i)
-            rob(io.rob_index_wb(i)).branch_target := io.branch_target_wb(i)
-            rob(io.rob_index_wb(i)).rf_wdata := io.rf_wdata_wb(i)
-            rob(io.rob_index_wb(i)).real_jump := io.real_jump_wb(i)
+            rob(io.rob_index_wb(i)(1, 0))(io.rob_index_wb(i)(log2Ceil(neach)+1, 2)).complete := true.B
+            rob(io.rob_index_wb(i)(1, 0))(io.rob_index_wb(i)(log2Ceil(neach)+1, 2)).predict_fail := io.predict_fail_wb(i)
+            rob(io.rob_index_wb(i)(1, 0))(io.rob_index_wb(i)(log2Ceil(neach)+1, 2)).branch_target := io.branch_target_wb(i)
+            rob(io.rob_index_wb(i)(1, 0))(io.rob_index_wb(i)(log2Ceil(neach)+1, 2)).rf_wdata := io.rf_wdata_wb(i)
+            rob(io.rob_index_wb(i)(1, 0))(io.rob_index_wb(i)(log2Ceil(neach)+1, 2)).real_jump := io.real_jump_wb(i)
         }
     }
     
     // cmt stage
-    io.cmt_en(0) := rob(head).complete && !empty
+    io.cmt_en(0) := rob(head_sel)(head(head_sel)).complete && !empty
     for(i <- 1 until 4){
-        io.cmt_en(i) := (io.cmt_en(i-1) && rob(head+i.U).complete && !rob(head+(i-1).U).predict_fail  
-                        && !rob(head+(i-1).U).is_store && !rob(head+(i-1).U).pred_update_en)
+        io.cmt_en(i) := (io.cmt_en(i-1) && rob(head_sel+i.U)(head(head_sel+i.U)).complete && !rob(head_sel+(i-1).U)(head(head_sel+(i-1).U)).predict_fail  
+                        && !rob(head_sel+(i-1).U)(head(head_sel+(i-1).U)).is_store && !rob(head_sel+(i-1).U)(head(head_sel+(i-1).U)).pred_update_en)
     }
     io.full := full
     val predict_fail_bit = VecInit(Seq.fill(4)(false.B))
     val pred_update_en_bit = VecInit(Seq.fill(4)(false.B))
     for(i <- 0 until 4){
-        predict_fail_bit(i) := rob(head+i.U).predict_fail && io.cmt_en(i)
-        pred_update_en_bit(i) := rob(head+i.U).pred_update_en && io.cmt_en(i)
+        predict_fail_bit(i) := rob(head_sel+i.U)(head(head_sel+i.U)).predict_fail && io.cmt_en(i)
+        pred_update_en_bit(i) := rob(head_sel+i.U)(head(head_sel+i.U)).pred_update_en && io.cmt_en(i)
     }
     val pred_fail_ohbit = PriorityEncoder(predict_fail_bit.asUInt)
     val pred_update_ohbit = PriorityEncoder(pred_update_en_bit.asUInt)
     io.predict_fail_cmt := predict_fail_bit.asUInt.orR
-    io.branch_target_cmt := Mux(rob(head+pred_fail_ohbit).real_jump, 
-                                rob(head+pred_fail_ohbit).branch_target,
-                                rob(head+pred_fail_ohbit).pc + 4.U)
+    io.branch_target_cmt := Mux(rob(head_sel+pred_fail_ohbit)(head(head_sel+pred_fail_ohbit)).real_jump, 
+                                rob(head_sel+pred_fail_ohbit)(head(head_sel+pred_fail_ohbit)).branch_target,
+                                rob(head_sel+pred_fail_ohbit)(head(head_sel+pred_fail_ohbit)).pc + 4.U)
     io.pred_update_en_cmt := pred_update_en_bit.asUInt.orR
-    io.pred_branch_target_cmt := rob(head+pred_update_ohbit).branch_target
-    io.pred_pc_cmt := rob(head+pred_update_ohbit).pc
-    io.pred_real_jump_cmt := rob(head+pred_update_ohbit).real_jump
-
-    head := head + PopCount(io.cmt_en)
+    io.pred_branch_target_cmt := rob(head_sel+pred_update_ohbit)(head(head_sel+pred_update_ohbit)).branch_target
+    io.pred_pc_cmt := rob(head_sel+pred_update_ohbit)(head(head_sel+pred_update_ohbit)).pc
+    io.pred_real_jump_cmt := rob(head_sel+pred_update_ohbit)(head(head_sel+pred_update_ohbit)).real_jump
 
     for(i <- 0 until 4){
-        io.rd_cmt(i) := rob(head+i.U).rd
-        io.rd_valid_cmt(i) := rob(head+i.U).rd_valid
-        io.prd_cmt(i) := rob(head+i.U).prd
-        io.pprd_cmt(i) := rob(head+i.U).pprd
-        io.pc_cmt(i) := Mux(rob(head+i.U).real_jump, rob(head+i.U).branch_target, rob(head+i.U).pc+4.U)
-        io.rf_wdata_cmt(i) := rob(head+i.U).rf_wdata
-        io.is_store_cmt(i) := rob(head+i.U).is_store && io.cmt_en(i)
+        io.rd_cmt(i) := rob(head_sel+i.U)(head(head_sel+i.U)).rd
+        io.rd_valid_cmt(i) := rob(head_sel+i.U)(head(head_sel+i.U)).rd_valid
+        io.prd_cmt(i) := rob(head_sel+i.U)(head(head_sel+i.U)).prd
+        io.pprd_cmt(i) := rob(head_sel+i.U)(head(head_sel+i.U)).pprd
+        io.pc_cmt(i) := Mux(rob(head_sel+i.U)(head(head_sel+i.U)).real_jump, rob(head_sel+i.U)(head(head_sel+i.U)).branch_target, rob(head_sel+i.U)(head(head_sel+i.U)).pc+4.U)
+        io.rf_wdata_cmt(i) := rob(head_sel+i.U)(head(head_sel+i.U)).rf_wdata
+        io.is_store_cmt(i) := rob(head_sel+i.U)(head(head_sel+i.U)).is_store && io.cmt_en(i)
     }
+
+    head_sel := Mux(io.predict_fail_cmt, 0.U, head_sel + PopCount(io.cmt_en))
+    for(i <- 0 until 4){
+        head(head_sel+i.U) := Mux(io.predict_fail_cmt, 0.U, head(head_sel+i.U) + io.cmt_en(i))
+    }
+    tail := Mux(io.predict_fail_cmt, 0.U, Mux(!full && !io.stall, tail + io.inst_valid_rn.asUInt.orR, tail))
+    elem_num := Mux(io.predict_fail_cmt, 0.U, Mux(!full && !io.stall, elem_num + insert_num - PopCount(io.cmt_en), elem_num - PopCount(io.cmt_en)))
+
+    // import ROB_Pack._
+    // val rob = RegInit(VecInit(Seq.fill(n)(0.U.asTypeOf(new rob_t))))
+
+    // val head = RegInit(0.U(log2Ceil(n).W))
+    // val tail = RegInit(0.U(log2Ceil(n).W))
+    // val elem_num = RegInit(0.U((log2Ceil(n)+1).W))
+
+    // val empty = elem_num === 0.U
+    // val insert_num = PopCount(io.inst_valid_rn)
+    // val full = elem_num + insert_num > n.asUInt
+
+    // when(~full){
+    //     for(i <- 0 until 4){
+    //         when(io.inst_valid_rn(i)){
+    //             rob(tail+i.U).rd := io.rd_rn(i)
+    //             rob(tail+i.U).rd_valid := io.rd_valid_rn(i)
+    //             rob(tail+i.U).prd := io.prd_rn(i)
+    //             rob(tail+i.U).pprd := io.pprd_rn(i)
+    //             rob(tail+i.U).pc := io.pc_rn(i)
+    //             rob(tail+i.U).is_store := io.is_store_rn(i)
+    //             rob(tail+i.U).pred_update_en := io.pred_update_en_rn(i)
+    //             rob(tail+i.U).rf_wdata := 0.U
+    //             rob(tail+i.U).real_jump := false.B
+    //             rob(tail+i.U).predict_fail := false.B
+    //             rob(tail+i.U).branch_target := 0.U
+    //             rob(tail+i.U).complete := false.B
+    //         }
+    //     }
+    // }
+
+    // tail := Mux(io.predict_fail_cmt, head + PopCount(io.cmt_en), Mux(!full && !io.stall, tail + insert_num, tail))
+    // elem_num := Mux(io.predict_fail_cmt, 0.U, Mux(!full && !io.stall, elem_num + insert_num - PopCount(io.cmt_en), elem_num - PopCount(io.cmt_en)))
+    // for(i <- 0 until 4){
+    //     io.rob_index_rn(i) := tail + i.U
+    // }
+    
+    // // wb stage
+    // for(i <- 0 until 4){
+    //     when(io.inst_valid_wb(i)){
+    //         rob(io.rob_index_wb(i)).complete := true.B
+    //         rob(io.rob_index_wb(i)).predict_fail := io.predict_fail_wb(i)
+    //         rob(io.rob_index_wb(i)).branch_target := io.branch_target_wb(i)
+    //         rob(io.rob_index_wb(i)).rf_wdata := io.rf_wdata_wb(i)
+    //         rob(io.rob_index_wb(i)).real_jump := io.real_jump_wb(i)
+    //     }
+    // }
+    
+    // // cmt stage
+    // io.cmt_en(0) := rob(head).complete && !empty
+    // for(i <- 1 until 4){
+    //     io.cmt_en(i) := (io.cmt_en(i-1) && rob(head+i.U).complete && !rob(head+(i-1).U).predict_fail  
+    //                     && !rob(head+(i-1).U).is_store && !rob(head+(i-1).U).pred_update_en)
+    // }
+    // io.full := full
+    // val predict_fail_bit = VecInit(Seq.fill(4)(false.B))
+    // val pred_update_en_bit = VecInit(Seq.fill(4)(false.B))
+    // for(i <- 0 until 4){
+    //     predict_fail_bit(i) := rob(head+i.U).predict_fail && io.cmt_en(i)
+    //     pred_update_en_bit(i) := rob(head+i.U).pred_update_en && io.cmt_en(i)
+    // }
+    // val pred_fail_ohbit = PriorityEncoder(predict_fail_bit.asUInt)
+    // val pred_update_ohbit = PriorityEncoder(pred_update_en_bit.asUInt)
+    // io.predict_fail_cmt := predict_fail_bit.asUInt.orR
+    // io.branch_target_cmt := Mux(rob(head+pred_fail_ohbit).real_jump, 
+    //                             rob(head+pred_fail_ohbit).branch_target,
+    //                             rob(head+pred_fail_ohbit).pc + 4.U)
+    // io.pred_update_en_cmt := pred_update_en_bit.asUInt.orR
+    // io.pred_branch_target_cmt := rob(head+pred_update_ohbit).branch_target
+    // io.pred_pc_cmt := rob(head+pred_update_ohbit).pc
+    // io.pred_real_jump_cmt := rob(head+pred_update_ohbit).real_jump
+
+    // head := head + PopCount(io.cmt_en)
+
+    // for(i <- 0 until 4){
+    //     io.rd_cmt(i) := rob(head+i.U).rd
+    //     io.rd_valid_cmt(i) := rob(head+i.U).rd_valid
+    //     io.prd_cmt(i) := rob(head+i.U).prd
+    //     io.pprd_cmt(i) := rob(head+i.U).pprd
+    //     io.pc_cmt(i) := Mux(rob(head+i.U).real_jump, rob(head+i.U).branch_target, rob(head+i.U).pc+4.U)
+    //     io.rf_wdata_cmt(i) := rob(head+i.U).rf_wdata
+    //     io.is_store_cmt(i) := rob(head+i.U).is_store && io.cmt_en(i)
+    // }
 } 
 
 // object ROB extends App {
