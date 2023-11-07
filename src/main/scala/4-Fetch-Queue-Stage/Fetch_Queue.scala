d0b1277f239dc69e31516a7f744ceae011268ecd
MaZirui <454434635@qq.com>
2023-11-07 20:30:48 +0800

    update fq

--- src/main/scala/4-Fetch-Queue-Stage/Fetch_Queue.scala
+++ src/main/scala/4-Fetch-Queue-Stage/Fetch_Queue.scala
@@ -32,40 +32,32 @@ class Fetch_Queue extends Module{
     val row_width = num_entries / fetch_width
     val queue = RegInit(VecInit(Seq.fill(fetch_width)(VecInit(Seq.fill(row_width)(0.U.asTypeOf(new inst_pack_PD_t))))))
 
-    val head = RegInit((1.U(row_width.W)))
-    val head_index = OHToUInt(head)
-    val tail = RegInit((1.U(num_entries.W)))
-    val tail_index = OHToUInt(tail)
+    val head = RegInit(0.U(log2Ceil(row_width).W))
+    val tail = RegInit(0.U(log2Ceil(num_entries).W))
 
-    val full = head_index === tail_index(log2Ceil(num_entries)-1, log2Ceil(fetch_width)) + 1.U
-    val empty = head_index === tail_index(log2Ceil(num_entries)-1, log2Ceil(fetch_width))
+    val full  = head === tail(log2Ceil(num_entries)-1, log2Ceil(fetch_width)) + 1.U
+    val empty = head === tail(log2Ceil(num_entries)-1, log2Ceil(fetch_width))
 
 
     // Enqueue
     io.inst_queue_ready := !full
 
-    def inc(ptr: UInt) = {
-        val n = ptr.getWidth
-        Cat(ptr(n-2, 0), ptr(n-1))
-    }
     // calculate the entry index for each instruction
-    val entry_idxs = Wire(Vec(fetch_width, UInt(num_entries.W)))
+    val entry_idxs = Wire(Vec(fetch_width, UInt(log2Ceil(num_entries).W)))
     var entry_index = tail
     for(i <- 0 until fetch_width){
         entry_idxs(i) := entry_index
-        entry_index = Mux(io.insts_pack(i).inst_valid, inc(entry_index), entry_index)
+        entry_index = Mux(io.insts_pack(i).inst_valid, entry_index + 1.U, entry_index)
     }
     // write to queue
     for(i <- 0 until fetch_width){
-        for(j <- 0 until num_entries){
-            when(!full && io.insts_pack(i).inst_valid && entry_idxs(i)(j)){
-                queue(j % fetch_width)(j / fetch_width) := io.insts_pack(i)
-            }
+        when(!full && io.insts_pack(i).inst_valid){
+            queue(entry_idxs(i)(log2Ceil(fetch_width)-1, 0))(entry_idxs(i)(log2Ceil(num_entries)-1, log2Ceil(fetch_width))) := io.insts_pack(i)
         }
     }
     // Dequeue
     for(i <- 0 until fetch_width){
-        io.insts_pack_id(i) := queue(i)(head_index)
+        io.insts_pack_id(i) := queue(i)(head)
         io.insts_valid_decode(i) := !empty
     }
     // update ptrs
@@ -73,11 +65,11 @@ class Fetch_Queue extends Module{
         tail := entry_index
     }
     when(io.next_ready && !empty){
-        head := inc(head)
+        head := head + 1.U
     }
     when(io.flush){
-        head := 1.U
-        tail := 1.U
+        head := 0.U
+        tail := 0.U
     }
 
 
