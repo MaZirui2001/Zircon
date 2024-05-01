6054b982db9bff968dbb666cd17d7b320e62d17d
madrid <454434635@qq.com>
2024-05-01 23:06:35 +0800

    update timing

--- src/main/scala/03-Previous-Decode-Stage/Fetch_Queue.scala
+++ src/main/scala/03-Previous-Decode-Stage/Fetch_Queue.scala
@@ -21,47 +21,87 @@ class Fetch_Queue extends Module{
     val ROW_WIDTH = FQ_NUM / 2
     val queue = RegInit(VecInit.fill(2)(VecInit.fill(ROW_WIDTH)(0.U.asTypeOf(new inst_pack_PD_t))))
 
-    val head = RegInit(VecInit.fill(2)(0.U(log2Ceil(ROW_WIDTH).W)))
-    val tail = RegInit(0.U(log2Ceil(FQ_NUM).W))
+    // val head = RegInit(VecInit.fill(2)(0.U(log2Ceil(ROW_WIDTH).W)))
+    // val tail = RegInit(0.U(log2Ceil(FQ_NUM).W))
+    def shift_add1(x: UInt): UInt = {
+        val n = x.getWidth
+        x(n-2, 0) ## x(n-1)
+    }
+    def shift_add2(x: UInt): UInt = {
+        val n = x.getWidth
+        x(n-3, 0) ## x(n-1, n-2)
+    }
+    val head = RegInit(1.U(ROW_WIDTH.W))
+    val tail = RegInit(1.U(FQ_NUM.W))
+    val tail_odd = VecInit.tabulate(ROW_WIDTH)(i => tail(2*i+1)).asUInt 
+    val tail_even = VecInit.tabulate(ROW_WIDTH)(i => tail(2*i)).asUInt
+    val tail_for_compare = tail_odd | tail_even
 
-    val full  = !(head(0) ^ (tail(log2Ceil(FQ_NUM)-1, 1) + 1.U))
-    val empty = !(head(1) ^ tail(log2Ceil(FQ_NUM)-1, 1))
+    val full = (head & shift_add1(tail_for_compare)).orR
+    val empty = (head & tail_for_compare).orR
+
+    // val full  = !(head(0) ^ (tail(log2Ceil(FQ_NUM)-1, 1) + 1.U))
+    // val empty = !(head(1) ^ tail(log2Ceil(FQ_NUM)-1, 1))
 
 
     // Enqueue
     io.full := full
 
     // calculate the entry index for each instruction
-    val entry_idxs = Wire(Vec(2, UInt(log2Ceil(FQ_NUM).W)))
-    var entry_index = tail
-    for(i <- 0 until 2){
-        entry_idxs(i) := entry_index
-        entry_index = Mux(io.insts_pack(i).inst_valid, entry_index + 1.U, entry_index)
-    }
-    // write to queue
-    for(i <- 0 until 2){
-        when(!full && io.insts_pack(i).inst_valid){
-            queue(entry_idxs(i)(0))(entry_idxs(i)(log2Ceil(FQ_NUM)-1, 1)) := io.insts_pack(i)
+    // val entry_idxs = Wire(Vec(2, UInt(log2Ceil(FQ_NUM).W)))
+    // var entry_index = tail
+    // for(i <- 0 until 2){
+    //     entry_idxs(i) := entry_index
+    //     entry_index = Mux(io.insts_pack(i).inst_valid, entry_index + 1.U, entry_index)
+    // }
+    val write_mask = tail & Fill(FQ_NUM, io.insts_pack(0).inst_valid) | shift_add1(tail) & Fill(FQ_NUM, io.insts_pack(1).inst_valid)
+    val write_mask_odd = VecInit.tabulate(ROW_WIDTH)(i => write_mask(2*i+1)).asUInt
+    val write_mask_even = VecInit.tabulate(ROW_WIDTH)(i => write_mask(2*i)).asUInt
+
+    val write_data_even = Mux(tail_even.orR, io.insts_pack(0), io.insts_pack(1))
+    val write_data_odd = Mux(!tail_even.orR, io.insts_pack(0), io.insts_pack(1))
+
+
+    // // write to queue
+    // for(i <- 0 until 2){
+    //     when(!full && io.insts_pack(i).inst_valid){
+    //         queue(entry_idxs(i)(0))(entry_idxs(i)(log2Ceil(FQ_NUM)-1, 1)) := io.insts_pack(i)
+    //     }
+    // }
+    // even queue write
+    for(i <- 0 until ROW_WIDTH){
+        when(!full && write_mask_even(i)){
+            queue(0)(i) := write_data_even
         }
     }
+    // odd queue write
+    for(i <- 0 until ROW_WIDTH){
+        when(!full && write_mask_odd(i)){
+            queue(1)(i) := write_data_odd
+        }
+    }
+
     // Dequeue
     for(i <- 0 until 2){
-        io.insts_pack_id(i) := queue(i)(head(i))
+        io.insts_pack_id(i) := Mux1H(head, queue(i))
         io.insts_valid_decode(i) := !empty
     }
     // update ptrs
     when(!full){
-        tail := entry_index
+        tail := Mux(io.insts_pack(0).inst_valid, Mux(io.insts_pack(1).inst_valid, shift_add2(tail), shift_add1(tail)), tail)
     }
     when(io.next_ready && !empty){
         // head := head + 1.U
-        for(i <- 0 until 2){
-            head(i) := head(i) + 1.U
-        }
+        // for(i <- 0 until 2){
+        //     head(i) := head(i) + 1.U
+        // }
+        head := shift_add1(head)
     }
     when(io.flush){
-        head.foreach(_ := 0.U)
-        tail := 0.U
+        // head.foreach(_ := 0.U)
+        // tail := 0.U
+        head := 1.U
+        tail := 1.U
     }
 
 
--- src/main/scala/04-Decode-Stage/Free_List.scala
+++ src/main/scala/04-Decode-Stage/Free_List.scala
@@ -17,47 +17,58 @@ class Free_List(n: Int) extends Module{
         val head_arch           = Input(UInt(log2Ceil(n).W))
     })
 
+    def shift_add1(x: UInt): UInt = {
+        val n = x.getWidth
+        x(n-2, 0) ## x(n-1)
+    }
     val free_list   = RegInit(VecInit.tabulate(n)(i => (i + 1).asUInt(log2Ceil(n)-1, 0)))
-    val tail        = RegInit((n - 1).U(log2Ceil(n).W))
-    val head        = RegInit(0.U(log2Ceil(n).W))
-    val head_plus_1 = RegInit(1.U(log2Ceil(n).W))
+    // val tail        = RegInit((n - 1).U(log2Ceil(n).W))
+    // val head        = RegInit(0.U(log2Ceil(n).W))
+    // val head_plus_1 = RegInit(1.U(log2Ceil(n).W))
+    val head = RegInit(1.U(n.W))
 
-    val head_empty  = VecInit(head, head_plus_1)
+    // 初始化，最高位是1，其余是0
+    val tail_init = VecInit.tabulate(n)(i => (i.U === (n-1).U)).asUInt
+    val tail = Reg(UInt(n.W))
+
+
+    val head_empty  = VecInit(head, shift_add1(head))
 
 
     // io.empty        := VecInit.tabulate(2)(i => Mux(head + i.U >= n.U, head + i.U - n.U, head + i.U) === tail).asUInt.orR
-    io.empty        := VecInit.tabulate(2)(i => head_empty(i) === tail).asUInt.orR
+    io.empty        := VecInit.tabulate(2)(i => (head_empty(i) & tail).orR).asUInt.orR
 
     // alloc new reg
-    val head_idx    = Wire(Vec(2, UInt(log2Ceil(n).W)))
+    val head_idx    = Wire(Vec(2, UInt(n.W)))
     var head_now    = head
     io.alloc_preg   := VecInit.fill(2)(0.U(log2Ceil(n).W))
     for(i <- 0 until 2){
         head_idx(i)         := head_now
-        head_now            = Mux(io.rd_valid(i) && io.rename_en(i) && !io.empty, Mux(head_now === (n-1).U, 0.U, head_now + 1.U), head_now)
-        io.alloc_preg(i)    := free_list(head_idx(i))
+        head_now            = Mux(io.rd_valid(i) && io.rename_en(i) && !io.empty, shift_add1(head_now), head_now)
+        io.alloc_preg(i)    := Mux1H(head_idx(i), free_list)
     }
     head := head_now
-    head_plus_1 := Mux(head_now === (n-1).U, 0.U, head_now + 1.U)
     
 
     // commit old reg
-    val tail_idx    = Wire(Vec(2, UInt(log2Ceil(n).W)))
+    val tail_idx    = Wire(Vec(2, UInt(n.W)))
     var tail_now    = tail
     for(i <- 0 until 2){
         tail_idx(i)         := tail_now
-        tail_now            = Mux(io.commit_en(i) && io.commit_pprd_valid(i), Mux(tail_now === (n-1).U, 0.U, tail_now + 1.U), tail_now)
+        tail_now            = Mux(io.commit_en(i) && io.commit_pprd_valid(i), shift_add1(tail_now), tail_now)
         when(io.commit_en(i) && io.commit_pprd_valid(i)){
-            free_list(tail_idx(i)) := io.commit_pprd(i)
+            free_list(OHToUInt(tail_idx(i))) := io.commit_pprd(i)
         }
     }
-    
     tail := tail_now
+    when(reset.asBool){
+        tail := tail_init
+    }
 
 
     // flush
     when(io.predict_fail){
-        head := io.head_arch
+        head := UIntToOH(io.head_arch)
     }
 
 }
